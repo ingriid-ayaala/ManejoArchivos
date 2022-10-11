5e53b576ffb5641f85d0f51cadea976c913c7604
ingriid-ayaala <ingriidayaala@gmail.com>
2022-10-11 10:06:58 +0200

    tema nuevo

---
+++ manejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -0,0 +1,23 @@
+/*
+Crear un archivo
+Guardar informacion de un archivo
+Eliminar informacion de un archivo
+Eliminar un archivo
+
+*/
+package manejoarchivos;
+
+/**
+ *
+ * @author ingri
+ */
+public class ManejoArchivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
