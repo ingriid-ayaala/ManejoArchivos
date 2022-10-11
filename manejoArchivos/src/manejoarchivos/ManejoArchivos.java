494b84b919049c1a5522593f79941e5af4499adc
ingriid-ayaala <ingriidayaala@gmail.com>
2022-10-11 10:26:01 +0200

    crearArchivos

--- manejoArchivos/src/manejoarchivos/ManejoArchivos.java
+++ manejoArchivos/src/manejoarchivos/ManejoArchivos.java
@@ -7,6 +7,9 @@ Eliminar un archivo
 */
 package manejoarchivos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 /**
  *
  * @author ingri
@@ -16,8 +19,16 @@ public class ManejoArchivos {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+       
+    public static void crearArchivo(String nombre){
+        File archivo = new File(nombre);
+        try {
+            PrintWriter salida = new PrintWriter(archivo);
+            salida.close();
+        } catch (FileNotFoundException ex){
+            ex.printStackTrace(System.out);
+        }
     }
+    
     
 }
---
+++ manejoArchivos/src/principal/PPal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package principal;
+import manejoarchivos.*;
+/**
+ *
+ * @author ingri
+ */
+public class PPal {
+    public static void main (String[] args){
+        String nombreArchivo = "prueba.txt";
+        ManejoArchivos.crearArchivo(nombreArchivo);
+    }
+}
